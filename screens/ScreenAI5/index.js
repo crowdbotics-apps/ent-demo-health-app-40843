35bc0f8c4f6fba36c975d13b87149bf197ee0c30
Jason Kozel <jason.kozel@crowdbotics.com>
2023-05-19 09:26:57 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI5/index.js
+++ screens/ScreenAI5/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 const patientData = [{
   id: 1,
