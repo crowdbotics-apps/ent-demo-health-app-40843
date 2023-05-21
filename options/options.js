ab951735064f787a2ff3b234b35c40719690fb0c
Jason Kozel <jason.kozel@crowdbotics.com>
2023-05-21 15:20:16 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "ent-demo-health-ins-40843",
   "url": "https://ent-demo-health-ins-40843.botics.co",
   "api": "https://ent-demo-health-ins-40843.botics.co/api/v1",
-  "initialRoute": "Untitled1"
+  "initialRoute": "Untitled3"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
