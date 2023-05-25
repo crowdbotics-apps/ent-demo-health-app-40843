de127d79cf467b98c827c2049f77d6358cefa687
crowdbotics-apps <team@crowdbotics.com>
2023-05-25 16:57:50 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -25,6 +25,7 @@ class User(AbstractUser):
     medication = models.CharField(null=True,blank=True,max_length=256,)
     phone = models.CharField(null=True,blank=True,max_length=256,)
     email = models.EmailField(null=True,blank=True,max_length=254,)
+    patientId = models.CharField(null=True,blank=True,max_length=256,)
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
 class CallCenter(models.Model):
