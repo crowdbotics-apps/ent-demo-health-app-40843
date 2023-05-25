022b852327a6692d14e1a8a34bfb21ebf492ba88
crowdbotics-apps <team@crowdbotics.com>
2023-05-25 18:56:28 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -35,4 +35,4 @@ class CallCenter(models.Model):
     outbound = models.BooleanField()
     callNotes = models.TextField()
     supportRepName = models.CharField(max_length=256,)
-    patientId = models.ForeignKey("users.User",on_delete=models.CASCADE,null=True,blank=True,related_name="callcenter_patientId",)
+    patientId = models.ForeignKey("users.User",null=True,blank=True,on_delete=models.CASCADE,related_name="callcenter_patientId",)
