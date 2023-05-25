c6b3b57e58ab4c36f731b3531ca244c7fc7c6044
crowdbotics-apps <team@crowdbotics.com>
2023-05-25 16:51:20 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -34,4 +34,3 @@ class CallCenter(models.Model):
     outbound = models.BooleanField()
     callNotes = models.TextField()
     supportRepName = models.CharField(max_length=256,)
-    patientId = models.CharField(max_length=256,null=True,blank=True,)
